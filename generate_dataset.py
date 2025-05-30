7e126afcbcd5f5fe9019f28012f4235ae4166c3c
piliadis <petros_iliadis@hotmail.com>
2025-05-30 10:11:24 +0300

    add seed to dataset generation

--- generate_dataset.py
+++ generate_dataset.py
@@ -6,7 +6,7 @@ import os
 import random
 from scipy.sparse import csc_matrix
 
-network = "mynetwork"
+network = "123Bus"
 
 # se kathe fakelo exw ena myscript.dss pou kleinw tous tap changers.
 dss_file = "opendss/" + network + "/myscript.dss"
@@ -15,6 +15,7 @@ num_runs = 10000
 variation_low, variation_high = 0.8, 1.2
 initial_dir = os.getcwd()
 output_dir = "data/" + network
+random.seed(42)
 
 # ~~~~~~~~~~~~~~~~~~~~ GET Y ~~~~~~~~~~~~~~~~~~~~~
 dss.Command("Clear")
@@ -22,7 +23,10 @@ dss.Command(f"compile [{dss_file}]")
 
 dss.Text.Command("vsource.source.enabled=no")
 dss.Text.Command("batchedit load..* enabled=no")
-dss.Text.Command("batchedit capacitor..* enabled=no")
+# dss.Text.Command("batchedit capacitor..* enabled=no")  # den eimai sigouros an to xreiazomai
+# evgala tin line me to switch!!
+# evgala kai tous 3 regulators
+# dss.Text.Command("batchedit Transformer..* enabled=no") # ayto den doulevei. vgazei kai ton source..
 # dss.Text.Command("batchedit generator..* enabled=no")
 
 dss.Command("solve")
@@ -33,6 +37,8 @@ y_node_order = dss.Circuit.YNodeOrder()  # SWSTO
 # Extract Ybus matrix
 data, indices, indptr = dss.YMatrix.getYsparse(factor=False)
 Ybus = csc_matrix((data, indices, indptr)).toarray()
+# lol = dss.Circuit.SystemY() # to idio apla thelei reshaping
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -49,12 +55,15 @@ for run in range(num_runs):
     dss.Text.Command(f"compile [{initial_dir+'/'+dss_file}]")
     # breakpoint()
 
+    loads = []
     # === Randomize Load ===
     for load in dss.Loads.AllNames():
         dss.Loads.Name(load)
         factor = random.uniform(variation_low, variation_high)
         dss.Loads.kW(dss.Loads.kW() * factor)
-        dss.Loads.kvar(dss.Loads.kvar() * factor)
+        factor1 = random.uniform(variation_low, variation_high)
+        dss.Loads.kvar(dss.Loads.kvar() * factor1)
+        loads.append((load, dss.Loads.kW(), dss.Loads.kvar()))
 
     # === Solve Power Flow ===
     dss.Command("solve")
@@ -65,16 +74,37 @@ for run in range(num_runs):
     buses = dss.Circuit.YNodeOrder()
 
     # === Extract Voltages ===
-    V_array = dss.Circuit.YNodeVArray()
+    V_array = dss.Circuit.YNodeVArray()  # exei allaksei to order kai thelei ftiaksimo!!
     V_complex = np.array(
         [V_array[i] + 1j * V_array[i + 1] for i in range(0, len(V_array), 2)]
     )  # ayto einai to swsto!!
     V_df = pd.DataFrame([list(V_complex)], columns=buses)
     V = V_df[y_node_order].values.flatten()
 
+    # data, indices, indptr = dss.YMatrix.getYsparse(factor=False)
+    # Ybus1 = csc_matrix((data, indices, indptr)).toarray()
+
     # === Power Calculation ===
     I_calc = Ybus @ V
     S_calc = V * np.conj(I_calc)
+
+    # I_calc2 = np.conj(S_calc / V)  # equal to I_calc
+
+    # I_calc_ = dss.Circuit.YCurrents()
+    # I_calc2 = np.array([I_calc_[i] + 1j * I_calc_[i + 1] for i in range(0, len(I_calc_), 2)])  # den einai to idio me to I_calc. einai iso me -> Ybus1 @ V
+    # I_calc__ = dss.YMatrix.getI()
+    # I_calc3 = np.array([I_calc__[i] + 1j * I_calc__[i + 1] for i in range(0, len(I_calc__), 2)])  # idio me to I_calc2. apla exei parapanw stoixeio stin arxi
+    # breakpoint()
+
+    # === Bus Injections ===
+    # S_bus = []
+    # for bus in y_node_order:
+    #     dss.Circuit.SetActiveBus(bus)  # make this bus the active one
+
+    #     pq = np.array(dss.CktElement.Powers())  # [P1, Q1, P2, Q2, P3, Q3, …]  kW / kVAr
+    #     S_phase = pq[0] + 1j * pq[1]  # complex kVA per phase (kW + j kVAr)
+
+    #     S_bus.append((bus, S_phase * 1000))
 
     # === Store Results ===
     results["V_real"].append(V.real)
